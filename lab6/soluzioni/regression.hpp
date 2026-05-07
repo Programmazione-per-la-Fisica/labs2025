7db6829f6afeae03cb60d32e6e371c5e6635aa22
Tommaso Diotalevi <tommaso.diotalevi@gmail.com>
2026-05-07 16:49:47 +0200

    Add lab6 solutions

---
+++ lab6/soluzioni/regression.hpp
@@ -0,0 +1,46 @@
+#ifndef PF_REGRESSION_HPP
+#define PF_REGRESSION_HPP
+
+#include <vector>
+
+namespace pf {
+
+struct Result {
+  double A;
+  double B;
+};
+
+struct Point {
+  double x;
+  double y;
+};
+
+bool operator==(Point const&, Point const&);
+
+class Regression {
+  std::vector<Point> points_;
+
+ public:
+  std::size_t size() const;
+
+  const std::vector<Point>& points() const;
+
+  void add(Point const&);
+  void add(double x, double y);
+  void add(std::vector<Point> const&);
+
+  bool remove(Point const&);
+  bool remove(double x, double y);
+
+  Result fit() const;
+
+  Regression& operator+=(Regression const&);
+};
+
+Regression operator+(Regression const&, Regression const&);
+
+Result fit(Regression const&);
+
+}  // namespace pf
+
+#endif
